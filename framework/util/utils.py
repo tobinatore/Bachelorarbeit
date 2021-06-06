3129ba8e7d783267e150461618de73066ab1c9b1
Tobias Nöthlich <germanicalp@gmail.com>
2021-06-06 16:14:49 +0200

    Added decoding incoming bundles and getting their source

--- framework/util/utils.py
+++ framework/util/utils.py
@@ -1,2 +1,17 @@
-def parse_bundle(recvd_message: bytes) -> str:
-    print(recvd_message)
+from cbor2 import loads
+
+
+def get_bundle_source(recvd_message: bytes) -> str:
+    """Parses the received data and extracts the senders node number.
+
+    Args:
+        recvd_message (bytes): Bundle conforming to the Concise Binary Object Representation.
+
+    Returns:
+        str: The node number / name of the bundles origin.
+    """
+
+    # Decoding the byte-representation and accessing the node number of the sender.
+    # see https://datatracker.ietf.org/doc/html/draft-ietf-dtn-bpbis-31#section-4.3.1
+    # for the structure of the primary bundle block.
+    return loads(recvd_message)[0][4][1][0]
